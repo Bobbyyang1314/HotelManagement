fafd56e82a0bd967dcc1d1743c424e0876557675
Bobby Yang <90142233+Bobbyyang1314@users.noreply.github.com>
2023-11-11 00:39:43 -0500

    Initial commit

---
+++ HotelMan_HotelAdmin/HotelAdmin.cs
@@ -0,0 +1,107 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Model;
+using HotelMan_HotelAdmin.Models;
+using HttpMultipartParser;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace HotelMan_HotelAdmin;
+
+public class HotelAdmin
+{
+    public async Task<APIGatewayProxyResponse> AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        var response = new APIGatewayProxyResponse()
+        {
+            Headers = new Dictionary<string, string>(),
+            StatusCode = 200
+        };
+        
+        response.Headers.Add("Access-Control-Allow-Origin","*");
+        response.Headers.Add("Access-Control-Allow-Headers","*");
+        response.Headers.Add("Access-Control-Allow-Methods","OPTIONS,POST");
+
+        var bodyContent = request.IsBase64Encoded
+            ? Convert.FromBase64String(request.Body)
+            : Encoding.UTF8.GetBytes(request.Body);
+
+        using var memStream = new MemoryStream(bodyContent);
+        var formData = MultipartFormDataParser.Parse(memStream);
+
+        // Fronted name in the html
+        var hotelName = formData.GetParameterValue("hotelName");
+        var hotelRating = formData.GetParameterValue("hotelRating");
+        var hotelCity = formData.GetParameterValue("hotelCity");
+        var hotelPrice = formData.GetParameterValue("hotelPrice");
+
+        var file = formData.Files.FirstOrDefault();
+        var fileName = file.FileName;
+
+        await using var fileContentStream = new MemoryStream();
+        await file.Data.CopyToAsync(fileContentStream);
+        fileContentStream.Position = 0;
+        //file.Data
+
+        var userId = formData.GetParameterValue("UserId");
+        var idToken = formData.GetParameterValue("idToken");
+
+        var token = new JwtSecurityToken(jwtEncodedString: idToken);
+        var group = token.Claims.FirstOrDefault(x => x.Type == "cognito:groups");
+        if (group == null || group.Value != "Admin")
+        {
+            response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            response.Body = JsonSerializer.Serialize(new { Error = "Unauthorised. Must be a member of Admin group." });
+        }
+
+        var region = Environment.GetEnvironmentVariable("AWS_REGION");
+        var bucketName = Environment.GetEnvironmentVariable("bucketName");
+        
+        var client = new AmazonS3Client(RegionEndpoint.GetBySystemName(region));
+        var dbClient = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(region));
+
+        
+
+        try
+        {
+            await client.PutObjectAsync(new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = fileName,
+                InputStream = fileContentStream,
+                AutoCloseStream = true
+            });
+
+            var hotel = new Hotel
+            {
+                UserId = userId,
+                Id = Guid.NewGuid().ToString(),
+                Name = hotelName,
+                CityName = hotelCity,
+                Price = int.Parse(hotelPrice),
+                Rating = int.Parse(hotelRating),
+                FileName = fileName
+            };
+            // Store the infomation into dynamoDB
+            using var dbContext = new DynamoDBContext(dbClient);
+            await dbContext.SaveAsync(hotel);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        
+        Console.WriteLine("OK.");
+
+        return response;
+    }
+}
